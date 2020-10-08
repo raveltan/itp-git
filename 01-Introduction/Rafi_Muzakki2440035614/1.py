e19210e6751e1dda074ee591d4ca26e03936abd5
rafi <71486441+rafi06052002@users.noreply.github.com>
2020-10-08 21:59:36 +0700

    Update 1.py

--- 01-Introduction/Rafi_Muzakki2440035614/1.py
+++ 01-Introduction/Rafi_Muzakki2440035614/1.py
@@ -1,2 +1,16 @@
-print('hello world')
 
+print('my english score')
+value=int(input('my score='))
+if (value >=90 and  value <=100):
+    print('I passed the test and i feel inevitable')
+elif (value >=80 and value <=89):
+    print('I passed the test and i feel great')
+elif (value >=70 and value <=79):
+    print('I passed the test but i need to study harder')
+elif (value >=60 and value <=69):
+    print('I need to be  more focus but you tried your best,better luck next time')
+elif (value >=40 and value <=59):
+    print('You should try reviewing your studies and do not play games all the time')
+else:
+    print('This is a disaster and you will bounce back try to learn harder next time')
+    
