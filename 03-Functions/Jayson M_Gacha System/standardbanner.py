c95199f421857f2185ded93fc7c90cb393df43f2
JaysM32 <71362967+JaysM32@users.noreply.github.com>
2020-10-17 19:24:14 +0700

    Update standardbanner.py

--- 03-Functions/Jayson M_Gacha System/standardbanner.py
+++ 03-Functions/Jayson M_Gacha System/standardbanner.py
@@ -1,151 +1,144 @@
 import random
 
-DChoice = 0
-Guaranteed4 = 0 #number of pulls, when 10, u get 4-star
-Guaranteed5 = 0 #number of pulls, when 90, u get 5-star
-WeapTierlist = [3,4,5]
-Weaponlist = ["Sword", "Spear", "Halberd", "Pike", "Bow", "Gun", "Mace" , "Hammer", "Axe",
-              "Shuriken", "Kunai", "Greatsword(2-handed)", "Greathammer(2-handed)", "Throwing Knife"]
-CharaTierlist = [4,5]
-Charalist5 = ['Jean','Deluc','Venti','Klee','Mona','Xiao','Qiqi','Keqing']
-Charalist4 = ['Amber','Lisa','Kaeya','Barbara','Razor','Bennet','Noele','Fischl','Sucrose','Beidou','Ningguang','Xiangling','Xingqiu','Chongyun']
-total = 0
+
+Weap_Tier_list = [3,4,5] #constant
+Weapon_list = ["Sword", "Spear", "Halberd", "Pike", "Bow", "Gun", "Mace" , "Hammer", "Axe",
+              "Shuriken", "Kunai", "Greatsword(2-handed)", "Greathammer(2-handed)", "Throwing Knife"] #constant
+
+Chara_Tier_list = [4,5] #constant
+Chara_list_5 = ['Jean','Deluc','Venti','Klee','Mona','Xiao','Qiqi','Keqing'] #constant
+Chara_list_4 = ['Amber','Lisa','Kaeya','Barbara','Razor','Bennet','Noele','Fischl','Sucrose','Beidou','Ningguang','Xiangling','Xingqiu','Chongyun'] #constant
 
 
-def SinglePull():
-    global Guaranteed4, Guaranteed5, total
 
-    if Guaranteed5 >= 90:
-        GetChoice = random.randint(0, 2)
-        if GetChoice == 0:
+def SinglePull(pity4, pity5):
+
+    received = []
+
+    if pity5 == 90:
+        Get_Choice = random.randint(0, 2)
+        if Get_Choice == 0:
             Get_tier = [5]
-            Get = random.choice(Charalist5)
+            Get = random.choice(Chara_list_5)
         else:
             Get_tier = [5]
-            Get = random.choice(Weaponlist)
-        Guaranteed5 = 0
+            Get = random.choice(Weapon_list)
 
-    elif Guaranteed4 >= 10:
-        GetChoice2 = random.randint(0, 1)
-        if GetChoice2 == 0:
+    elif pity4 == 10:
+        Get_Choice_2 = random.randint(0, 1)
+        if Get_Choice_2 == 0:
             Get_tier = [4]
-            Get = random.choice(Charalist4)
+            Get = random.choice(Chara_list_4)
         else:
             Get_tier = [4]
-            Get = random.choice(Weaponlist)
+            Get = random.choice(Weapon_list)
 
     else:
-        Get_tier = random.choices(WeapTierlist, weights=[80, 15, 1])
-        Get = random.choice(Weaponlist)
-        #reset the counter if get certain tier
-        if Get_tier == 4:
-            Guaranteed4 = 0
-        elif Get_tier == 5:
-            Guaranteed5 = 0
+        Get_tier = random.choices(Weap_Tier_list, weights=[20, 5, 1])
+        Get = random.choice(Weapon_list)
 
-    #correction when using different pulls
-    if Guaranteed4 >= 10:
-        Guaranteed4 = 0
+    received.append([Get_tier , Get])
 
+    return received
 
-    Guaranteed4 += 1
-    Guaranteed5 += 1
-    total += 1
-    print("You Got", Get_tier,"-star", Get)
+def TenPull(pity5):
 
-def TenPull():
-    global Guaranteed4, Guaranteed5, total
     recieved = list()
 
-    if Guaranteed5 >= 90:
+    if pity5 == 90:
         #5starGuaranteed
-        GetChoice = random.randint(0,2)
-        if GetChoice == 0:
-            Charatier = [5]
-            Chara = random.choice(Charalist5)
-            recieved.append([Charatier, Chara])
+        Get_Choice = random.randint(0,2)
+        if Get_Choice == 0:
+            Chara_tier = [5]
+            Chara = random.choice(Chara_list_5)
+            recieved.append([Chara_tier, Chara])
         else:
-            Weaptier = [5]
-            Weapon = random.choice(Weaponlist)
-            recieved.append([Weaptier, Weapon])
-        Guaranteed5 = 0
+            Weap_tier = [5]
+            Weapon = random.choice(Weapon_list)
+            recieved.append([Weap_tier, Weapon])
 
         #4starGuaranteed
-        GetChoice2 = random.randint(0, 1)
-        if GetChoice2 == 0:
+        Get_Choice2 = random.randint(0, 1)
+        if Get_Choice2 == 0:
             Charatier = [4]
-            Chara = random.choice(Charalist4)
+            Chara = random.choice(Chara_list_4)
             recieved.append([Charatier, Chara])
         else:
             Weaptier = [4]
-            Weapon = random.choice(Weaponlist)
+            Weapon = random.choice(Weapon_list)
             recieved.append([Weaptier, Weapon])
 
-        # correction when using different pulls
-        if Guaranteed4 > 10:
-            Guaranteed4 = 0
 
         #the rest of the pull
         for i in range (8):
-            Weaptier = random.choices(WeapTierlist, weights=[80, 15, 1])
-            Weapon = random.choice(Weaponlist)
-            recieved.append([Weaptier, Weapon])
-            # reset the counter if get certain tier
-            if Weaptier == 4:
-                Guaranteed4 = 0
-            elif Weaptier == 5:
-                Guaranteed5 = 0
-    else:
-        # 4starGuaranteed
-        GetChoice2 = random.randint(0, 1)
-        if GetChoice2 == 0:
-            Charatier = [4]
-            Chara = random.choice(Charalist4)
-            recieved.append([Charatier, Chara])
-        else:
-            Weaptier = [4]
-            Weapon = random.choice(Weaponlist)
+            Weaptier = random.choices(Weap_Tier_list, weights=[20, 5, 1])
+            Weapon = random.choice(Weapon_list)
             recieved.append([Weaptier, Weapon])
 
-        # correction when using different pulls
-        if Guaranteed4 > 10:
-            Guaranteed4 = 0
+
+    else:
+        # 4starGuaranteed
+        Get_Choice2 = random.randint(0, 1)
+        if Get_Choice2 == 0:
+            Chara_tier = [4]
+            Chara = random.choice(Chara_list_4)
+            recieved.append([Chara_tier, Chara])
+        else:
+            Weap_tier = [4]
+            Weapon = random.choice(Weapon_list)
+            recieved.append([Weap_tier, Weapon])
+
 
         # the rest of the pull
         for i in range(9):
-            Weaptier = random.choices(WeapTierlist, weights=[80, 15, 1])
-            Weapon = random.choice(Weaponlist)
-            recieved.append([Weaptier, Weapon])
-            # reset the counter if get certain tier
-            if Weaptier == 4:
-                Guaranteed4 = 0
-            elif Weaptier == 5:
-                Guaranteed5 = 0
+            Weap_tier = random.choices(Weap_Tier_list, weights=[20, 5, 1])
+            Weapon = random.choice(Weapon_list)
+            recieved.append([Weap_tier, Weapon])
 
-    Guaranteed5 += 10
-    total += 1
-    # printing
-    for result in range(10):
-        print("You got", recieved[result][0], "-star", recieved[result][1])
+    return recieved
+
 
 
 def StandardDraw():
-    global DChoice
+    total = 0
+    Guaranteed_4 = 0
+    Guaranteed_5 = 0
+
     while True:
         print("----------Standard Banner----------")
-        #print("Guaranteed 4 =", Guaranteed4)
-        #print("Guaranteed 5 =", Guaranteed5)
-        print("total pulls = ", total)
+        print("total =", total)
+        print("Guaranteed 4 =", Guaranteed_4)
+        print("Guaranteed 5 =", Guaranteed_5)
         print("-----------------------------------")
         print("Your Choice of")
         print("1. 1x Draw")
         print("2. 10x Draw")
         print("type '3' to go back")
         print("-----------------------------------")
+
         DChoice = eval(input("Draw choice = "))
         if DChoice == 1:
-            SinglePull()
+            total += 1
+            Guaranteed_4 += 1
+            Guaranteed_5 += 1
+            Collect = SinglePull(Guaranteed_4,Guaranteed_5)
+            print("you got ", Collect[0][0],"-star ", Collect[0][1])
+            if Guaranteed_4 == 10:
+                Guaranteed_4 = 0
+            if Guaranteed_5 == 90:
+                Guaranteed_5 = 0
+
         elif DChoice == 2:
-            TenPull()
+            total += 10
+            Guaranteed_5 += 10
+            Collect10 = TenPull(Guaranteed_5)
+            print(Guaranteed_5)
+            for result in Collect10:
+                print("You got", result[0], "-star", result[1])
+            if Guaranteed_4 == 10:
+                Guaranteed_4 = 0
+            if Guaranteed_5 == 90:
+                Guaranteed_5 = 0
+
         elif DChoice == 3:
             break
